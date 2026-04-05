a45ad8e66710f0d916e6fca0798ab2affb1cb53c
mdshantosharker <mdshantosharker@gmail.com>
2026-04-05 15:27:31 +0600

    added js statements

---
+++ Js-fundamentals/Statements.js
@@ -0,0 +1,9 @@
+// what is statement?
+
+// ans:
+
+// A statement in programming is a single, complete command or instruction that tells the computer to perform a specific action, such as assigning a value, controlling flow, or declaring a variable.(প্রোগ্রামিং-এ স্টেটমেন্ট হলো একটি একক, সম্পূর্ণ কমান্ড বা নির্দেশনা যা কম্পিউটারকে কোনো নির্দিষ্ট কাজ করতে বলে, যেমন—কোনো মান নির্ধারণ করা, কার্যপ্রবাহ নিয়ন্ত্রণ করা, বা কোনো ভেরিয়েবল ঘোষণা করা।)
+
+
+// Example:
+let x = 5; // Declares 'x' and assigns it the value 5
