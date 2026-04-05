d5b5e15dc034c6af1fcca5768cc9226825738a22
mdshantosharker <mdshantosharker@gmail.com>
2026-04-05 16:24:59 +0600

    added js commnets

---
+++ Js-fundamentals/Comments.js
@@ -0,0 +1,12 @@
+// What are Comments?
+//  Comments হলো এমন কোডের অংশ যা কম্পিউটার execute করে না।
+
+// example:
+
+/*
+এটি multi-line comment
+একাধিক লাইন লেখা যায়
+debug বা explain করার জন্য
+*/
+
+// /**/ =>multiLine Comments
