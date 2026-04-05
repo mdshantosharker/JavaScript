c5995c3fb7287bc8036db2b1dbb7a15e5f2318b3
mdshantosharker <mdshantosharker@gmail.com>
2026-04-05 16:10:22 +0600

    added js syntax

---
+++ Js-fundamentals/Syntax.js
@@ -0,0 +1,32 @@
+// What is Literal?
+
+// Ans:
+//  Literal = fixed value (direct value) in code( তুমি কোডে যে value সরাসরি লিখো, সেটাই literal)
+
+// Example:let x = 10;
+
+// ----------------------------
+// What is a Variable?
+
+//  Variable = a named container that stores data (value)
+// Variable হলো একটি container-এর মতো, যেখানে data store করে রাখা হয়।
+
+// Example:let x = 10;
+
+// =>
+// x =variable
+
+// ----------------------------
+// What is a Expression?
+
+// Expression হলো এমন একটি কোড বা অংশ, যা evaluate হয়ে একটি মান (value) তৈরি করে।
+
+// Example:5 + 5
+// x + 5 → expression
+
+
+
+
+
+
+// --------------------------------------------------
